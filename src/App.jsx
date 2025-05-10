820d83927c8d844610c6d7061e78102af7f3f080
Tetiana-BCN <tetiana.v.a@gmail.com>
2025-05-10 23:53:57 +0200

    FEAT Create branch main to initialize the project

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import { useFormState } from 'react-dom';
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+
+
+
+      </div>
+    </>
+  )
+}
+
+export default App
