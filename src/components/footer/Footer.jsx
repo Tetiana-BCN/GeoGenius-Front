a54e3b264e9475b2dd404697fd543ce01e528a77
Tetiana-BCN <tetiana.v.a@gmail.com>
2025-05-11 21:52:13 +0200

    FIX css issues

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,17 +1,17 @@
-import styles from './Footer.css';
+import styles from './Footer.module.css';
 import Logo from '../../assets/logo.png';
 
 function Footer() {
   return (
     <footer className={styles.footer}>
-        <div>
-      <img className="logo" src={Logo}/>
+      <div>
+        <img className={styles.logo} src={Logo} alt="GeoGenius Logo" />
         <p className={styles.copyright}>
           © 2025 GeoGenius. All rights reserved.
         </p>
-        </div>
+      </div>
     </footer>
   );
-};
+}
 
 export default Footer;
