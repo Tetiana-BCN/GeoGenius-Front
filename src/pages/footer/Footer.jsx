be3990cc6c50394aaa3e41a1cb0e42e6f897dfec
Tetiana-BCN <tetiana.v.a@gmail.com>
2025-05-11 19:02:55 +0200

    FEAT Add css to Footer element

--- src/pages/footer/Footer.jsx
+++ src/pages/footer/Footer.jsx
@@ -1,14 +1,15 @@
 import styles from './Footer.css';
 import Logo from '../../assets/logo.png';
 
-const Footer = () => {
+function Footer() {
   return (
     <footer className={styles.footer}>
+        <div>
       <img className="logo" src={Logo}/>
         <p className={styles.copyright}>
           © 2025 GeoGenius. All rights reserved.
         </p>
-      
+        </div>
     </footer>
   );
 };
