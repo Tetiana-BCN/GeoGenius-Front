a62c134775d5ed1f60a46b085caf8c6561563425
Tetiana-BCN <tetiana.v.a@gmail.com>
2025-05-11 18:47:28 +0200

    FEAT Create Footer

---
+++ src/pages/footer/Footer.jsx
@@ -0,0 +1,15 @@
+import styles from './footer/Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <img src/>
+        <p className={styles.copyright}>
+          © 2025 GeoGenius. All rights reserved.
+        </p>
+      
+    </footer>
+  );
+};
+
+export default Footer;
