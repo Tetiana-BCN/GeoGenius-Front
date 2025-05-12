f77dffedfd1f558001f5e0d2e79f65fce984cdef
Tetiana-BCN <tetiana.v.a@gmail.com>
2025-05-13 01:20:43 +0200

    FEAT Create button component

---
+++ src/components/button/Button.jsx
@@ -0,0 +1,16 @@
+
+import styles from './Button.module.css';
+
+const Button = ({ label, onClick, type = "primary", disabled = false }) => {
+  return (
+    <button
+      className={`${styles.button} ${styles[type]}`}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {label}
+    </button>
+  );
+};
+
+export default Button;
